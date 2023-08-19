c0d38b5007a898c033013b308b7643d4c18ba098
Fassace <142697071+Fassace@users.noreply.github.com>
2023-08-19 15:41:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var Prev = document.getElementById("Prev");
+var Next = document.getElementById("Next");
+var thumbnail = document.getElementsByClassName("thumbnail");
+var head = document.getElementsById("head");
+var backgroundImg = new Array(
+  "small1.jpg",
+  "small2.jpg",
+);
+  Next.onclick = function(){ "small2.jpg";
+}
